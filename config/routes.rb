e4470471e8e98d12eebd19505fb7d6bbff5ccfdf
NicolasCst <95449242+NicoCst@users.noreply.github.com>
2022-05-03 20:00:01 +0200

    Correction erreur ajout du s sur routes users & politicians

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :user, only: [:new, :create]
-  resources :politician, only: [:index, :show, :new, :create]
+  resources :users, only: [:new, :create]
+  resources :politicians, only: [:index, :show, :new, :create]
 end

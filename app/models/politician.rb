ee43c077b6f8b8de0986a43c4df02bbc8ed678fc
NicolasCst <95449242+NicoCst@users.noreply.github.com>
2022-05-03 21:22:11 +0200

    reviews et bookings routes done

--- app/models/politician.rb
+++ app/models/politician.rb
@@ -1,3 +1,4 @@
 class Politician < ApplicationRecord
-
+  has_many :bookings
+  has_many :reviews
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,4 @@
 class Review < ApplicationRecord
+  belongs_to :politician
+  belongs_to :user
 end

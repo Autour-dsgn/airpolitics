1180ddf2a2cd12cebfe55a33eab71e5516455b73
Aurelie Tournay <info@aurelietournay.com>
2022-05-14 16:22:52 +0200

    last update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
-
+  # def after_sign_in_path_for(resource)
+  #   if params[:action] == "show"
+  #     @politician = Politician.find(params[:id])
+  #     redirect_to politician_path(@politician)
+  #   else
+  #     super
+  #   end
+  # end
 end
